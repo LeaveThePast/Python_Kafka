87013823ec8ab05dbb8352d914677de4195327da
LeaveThePast <Ramil.Budagli@yandex.ru>
2024-03-13 01:22:11 +0400

    Initial commit

---
+++ consumer.py
@@ -0,0 +1,25 @@
+from confluent_kafka import Consumer, KafkaError
+
+# Конфигурация Kafka
+conf = {'bootstrap.servers': 'localhost:9092', 'group.id': 'my_consumer_group', 'auto.offset.reset': 'earliest'}
+
+# Создание объекта Consumer
+consumer = Consumer(conf)
+
+# Подписка на тему "test_topic"
+consumer.subscribe(['Simple'])
+
+# Чтение и обработка сообщений
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            continue
+        else:
+            print(msg.error())
+            break
+
+    print('Получено сообщение: {}'.format(msg.value().decode('utf-8')))
---
+++ producer.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Producer
+
+# Конфигурация Kafka
+conf = {'bootstrap.servers': 'localhost:9092'}
+
+# Создание объекта Producer
+producer = Producer(conf)
+
+
+# Отправка сообщения
+def delivery_report(err, msg):
+    if err is not None:
+        print('Сообщение не было доставлено: {}'.format(err))
+    else:
+        print('Сообщение успешно доставлено: {}'.format(msg.value().decode('utf-8')))
+
+
+# Отправка сообщения в тему "test_topic"
+producer.produce('Simple', key='Simple', value='Привет, Kafka!', callback=delivery_report)
+
+# Ждем, пока все сообщения будут отправлены
+producer.flush()
